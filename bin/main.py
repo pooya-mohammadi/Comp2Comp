fb15ec2c06800b0721a8fc116ed8b79a04a6b148
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-08-20 10:31:39 +0200

    Fix naming

--- bin/main.py
+++ bin/main.py
@@ -4,12 +4,16 @@ from pathlib import Path
 from deep_utils import DirUtils
 
 directory_path = "/home/aicvi/projects/Data_TAVI_TB2"
-analysis_type = "spine_muscle_adipose_tissue"
+# analysis_type = "spine_muscle_adipose_tissue"
+analysis_type = "aaa"
 if __name__ == '__main__':
     for patient_dir in DirUtils.list_dir_full_path(directory_path, only_directories=True):
         for image_path in DirUtils.list_dir_full_path(patient_dir, interest_extensions=".gz"):
             print(f"[INFO] Working on {image_path}")
             output_path = Path(image_path.replace(".nii.gz", "")) / "comp2comp" / analysis_type
+            # if output_path.exists():
+            #     continue
             output_path.mkdir(exist_ok=True, parents=True)
             os.system(
-                f"{os.path.split(__file__)[0]}/C2C {analysis_type} --input_path {image_path} --input_type nifti --save_segmentations --output {output_path} --overwrite_outputs")
+                f"{os.path.split(__file__)[0]}/C2C {analysis_type} --input_path {image_path}"
+                f" --input_type nifti --save_segmentations --output {output_path} --overwrite_outputs")
--- comp2comp/utils/orientation.py
+++ comp2comp/utils/orientation.py
@@ -10,23 +10,26 @@ class ToCanonical(InferenceClass):
     def __init__(self):
         super().__init__()
 
-
     def __call__(self, inference_pipeline):
         """
         First dim goes from L to R.
         Second dim goes from P to A.
         Third dim goes from I to S.
         """
-        output_dir = inference_pipeline.output_dir
-        output_dir_segmentations = os.path.join(output_dir, "segmentations/")
+        output_dir_segmentations = os.path.join(inference_pipeline.output_dir, "segmentations/")
 
-        canonical_segmentation = nib.as_closest_canonical(
-            inference_pipeline.segmentation
-        )
-        canonical_medical_volume = nib.as_closest_canonical(
-            inference_pipeline.medical_volume
+        nib.save(
+            inference_pipeline.segmentation,
+            os.path.join(output_dir_segmentations, "original_spine_seg.nii.gz"),
         )
 
+        nib.save(
+            inference_pipeline.medical_volume,
+            os.path.join(output_dir_segmentations, "original_converted_dcm.nii.gz"),
+        )
+
+        canonical_segmentation = nib.as_closest_canonical(inference_pipeline.segmentation)
+        canonical_medical_volume = nib.as_closest_canonical(inference_pipeline.medical_volume)
 
         nib.save(
             canonical_segmentation,
@@ -37,20 +40,14 @@ class ToCanonical(InferenceClass):
             canonical_medical_volume,
             os.path.join(output_dir_segmentations, "canonical_converted_dcm.nii.gz"),
         )
-        nib.save(
-            inference_pipeline.segmentation,
-            os.path.join(output_dir_segmentations, "original_spine_seg.nii.gz"),
-        )
 
-        nib.save(
-            inference_pipeline.medical_volume,
-            os.path.join(output_dir_segmentations, "original_converted_dcm.nii.gz"),
-        )
-        # print(f"[INFO] medical volume: {nib.aff2axcodes(inference_pipeline.medical_volume)} -> canonical: {nib.aff2axcodes(canonical_medical_volume)}")
-        # print(f"[INFO] Segmentation: {nib.aff2axcodes(inference_pipeline.segmentation)} -> canonical: {nib.aff2axcodes(canonical_segmentation)}")
+        print(
+            f"[INFO] medical volume: {nib.aff2axcodes(inference_pipeline.medical_volume.affine)} -> canonical: {nib.aff2axcodes(canonical_medical_volume.affine)}")
+        print(
+            f"[INFO] Segmentation: {nib.aff2axcodes(inference_pipeline.segmentation.affine)} -> canonical: {nib.aff2axcodes(canonical_segmentation.affine)}")
+        # Make sure to replace the input
         inference_pipeline.segmentation = canonical_segmentation
         inference_pipeline.medical_volume = canonical_medical_volume
-        inference_pipeline.pixel_spacing_list = (
-            canonical_medical_volume.header.get_zooms()
-        )
+
+        inference_pipeline.pixel_spacing_list = (canonical_medical_volume.header.get_zooms())
         return {}
